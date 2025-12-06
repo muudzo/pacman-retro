0bf0aae584fe7252793441e6855e972195e01cf9
muudzo <tatendawalter62@gmail.com>
2025-12-06 23:20:54 +0800

    feat: introduce constants for screen dimensions and colors

---
+++ pacman_game/constants.py
@@ -0,0 +1,11 @@
+# Screen Dimensions
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 800
+FPS = 60
+
+# Colors (R, G, B)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 0)
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
